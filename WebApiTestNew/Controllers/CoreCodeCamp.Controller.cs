bfb6f80a26eeb0620ecd042a6ef6ef93f71b267b
Pranab Das <pranabdas16@gmail.com>
2022-05-26 22:01:54 -0400

    Add project files.

---
+++ WebApiTestNew/Controllers/CoreCodeCamp.Controller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiTestNew.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoreCodeCamp : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCamps()
+        {
+
+            if (false) return BadRequest("Bad stuff Happen");
+
+            return Ok(new { Moniker = "ATL2018", Name = "Atlanta code camp" });
+        }
+    }
+}
